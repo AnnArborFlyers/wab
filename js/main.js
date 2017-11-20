1df1bca493450793baf4552cd13a19da39bc5714
Jason Hurd <jhurd@arbor.net>
2017-11-20 16:07:38 -0500

    broke it, back to working now

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ function (bootstrap, d3, nv, Aircraft, ich, template, list) {
         .enter()
           .append('option')
           .classed('opt', true)
-          .attr('value', function (aircraft) { return aircraftList.indexOf(N5337S); })
+          .attr('value', function (aircraft) { return aircraftList.indexOf(aircraft); })
           .text(function (aircraft) {
             return aircraft.code;
           });
