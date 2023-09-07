988b02ad0ca5016f7e4bb6a9fca599c345c7a60c
jasonhurd1 <me@jasonhurd.com>
2023-09-07 11:52:00 -0400

    Comment out change for now, is broken

--- js/main.js
+++ js/main.js
@@ -63,11 +63,11 @@ function (bootstrap, d3, nv, Aircraft, ich, template, list) {
         update(aircraftList[this.value]);
       });
 
-      var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
-        for(var i = 0; i < hashParams.length; i++){
-      var p = hashParams[i].split('=');
-      document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
-      }
+      //var hashParams = window.location.hash.substr(1).split('&'); // substr(1) to remove the `#`
+      //  for(var i = 0; i < hashParams.length; i++){
+      //var p = hashParams[i].split('=');
+      //document.getElementById(p[0]).value = decodeURIComponent(p[1]);;
+      //}
    } 
 
 
